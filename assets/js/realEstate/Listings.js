f2c39fe03d8658ff82c11e1f415e2edd15808e08
James <renatovisuals@gmail.com>
2018-10-18 02:20:59 -0500

    started styling real estate listing

--- assets/js/realEstate/Listings.js
+++ assets/js/realEstate/Listings.js
@@ -10,8 +10,68 @@ export default class Header extends Component {
   }
 
   render () {
-    return (<section className="listings">
-      Listings
+    return (<section id="listings" className="listings">
+      <section className="search-area">
+        <input type="text" name="search" />
+      </section>
+
+      <section className="sortby-area">
+        <div div className="results"> 390 results found </div>
+        <div className ="sort-options">
+          <select name="sortby" className="sortby">
+            <option value = "price-asc">Highest Price</option>
+            <option value = "price-asc">Highest Price</option>
+          </select>
+        <div className = "view">
+          <i className="fa fa-list-ul"></i>
+          <i className="fa fa-th"></i>
+        </div>
+
+        </div>
+      </section>
+
+      <section className="listings-results">
+        <div className="listing">
+          <div className="listing-img">
+            <span className="Address">Address</span>
+            <div className="details">
+              <div className="user-img"> </div>
+              <div className="user-details">
+                <span className="user-name">Nina Smith</span>
+                <span className="user-name">05/05/2018</span>
+              </div>
+              <div className="listing-details">
+                <div className="floor-space">
+                  <i className="fa fa-th"></i>
+                </div>
+                <span>1000ft&sup2;</span>
+              </div>
+                <div className="bedrooms">
+                  <i className="fa fa-bed"></i>
+                  <span>3 bedrooms</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bottom-info">
+            <span>$1000 / month</span>
+            <span><i className="fa fa-map-marker"></i></span>
+            <span>Ridgewood, NY</span>
+          </div>
+      </section>
+
+      <section classname="pagination">
+        <ul className="pagination-nums">
+          <li>Prev</li>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>Next</li>
+        </ul>
+
+      </section>
     </section>)
   }
 }
--- public/js/components/realEstate.js
+++ public/js/components/realEstate.js
@@ -364,8 +364,171 @@ var Header = function (_Component) {
     value: function render() {
       return _react2.default.createElement(
         'section',
-        { className: 'listings' },
-        'Listings'
+        { id: 'listings', className: 'listings' },
+        _react2.default.createElement(
+          'section',
+          { className: 'search-area' },
+          _react2.default.createElement('input', { type: 'text', name: 'search' })
+        ),
+        _react2.default.createElement(
+          'section',
+          { className: 'sortby-area' },
+          _react2.default.createElement(
+            'div',
+            { div: true, className: 'results' },
+            ' 390 results found '
+          ),
+          _react2.default.createElement(
+            'div',
+            { className: 'sort-options' },
+            _react2.default.createElement(
+              'select',
+              { name: 'sortby', className: 'sortby' },
+              _react2.default.createElement(
+                'option',
+                { value: 'price-asc' },
+                'Highest Price'
+              ),
+              _react2.default.createElement(
+                'option',
+                { value: 'price-asc' },
+                'Highest Price'
+              )
+            ),
+            _react2.default.createElement(
+              'div',
+              { className: 'view' },
+              _react2.default.createElement('i', { className: 'fa fa-list-ul' }),
+              _react2.default.createElement('i', { className: 'fa fa-th' })
+            )
+          )
+        ),
+        _react2.default.createElement(
+          'section',
+          { className: 'listings-results' },
+          _react2.default.createElement(
+            'div',
+            { className: 'listing' },
+            _react2.default.createElement(
+              'div',
+              { className: 'listing-img' },
+              _react2.default.createElement(
+                'span',
+                { className: 'Address' },
+                'Address'
+              ),
+              _react2.default.createElement(
+                'div',
+                { className: 'details' },
+                _react2.default.createElement(
+                  'div',
+                  { className: 'user-img' },
+                  ' '
+                ),
+                _react2.default.createElement(
+                  'div',
+                  { className: 'user-details' },
+                  _react2.default.createElement(
+                    'span',
+                    { className: 'user-name' },
+                    'Nina Smith'
+                  ),
+                  _react2.default.createElement(
+                    'span',
+                    { className: 'user-name' },
+                    '05/05/2018'
+                  )
+                ),
+                _react2.default.createElement(
+                  'div',
+                  { className: 'listing-details' },
+                  _react2.default.createElement(
+                    'div',
+                    { className: 'floor-space' },
+                    _react2.default.createElement('i', { className: 'fa fa-th' })
+                  ),
+                  _react2.default.createElement(
+                    'span',
+                    null,
+                    '1000ft\xB2'
+                  )
+                ),
+                _react2.default.createElement(
+                  'div',
+                  { className: 'bedrooms' },
+                  _react2.default.createElement('i', { className: 'fa fa-bed' }),
+                  _react2.default.createElement(
+                    'span',
+                    null,
+                    '3 bedrooms'
+                  )
+                )
+              )
+            )
+          ),
+          _react2.default.createElement(
+            'div',
+            { className: 'bottom-info' },
+            _react2.default.createElement(
+              'span',
+              null,
+              '$1000 / month'
+            ),
+            _react2.default.createElement(
+              'span',
+              null,
+              _react2.default.createElement('i', { className: 'fa fa-map-marker' })
+            ),
+            _react2.default.createElement(
+              'span',
+              null,
+              'Ridgewood, NY'
+            )
+          )
+        ),
+        _react2.default.createElement(
+          'section',
+          { classname: 'pagination' },
+          _react2.default.createElement(
+            'ul',
+            { className: 'pagination-nums' },
+            _react2.default.createElement(
+              'li',
+              null,
+              'Prev'
+            ),
+            _react2.default.createElement(
+              'li',
+              null,
+              '1'
+            ),
+            _react2.default.createElement(
+              'li',
+              null,
+              '2'
+            ),
+            _react2.default.createElement(
+              'li',
+              null,
+              '3'
+            ),
+            _react2.default.createElement(
+              'li',
+              null,
+              '4'
+            ),
+            _react2.default.createElement(
+              'li',
+              null,
+              '5'
+            ),
+            _react2.default.createElement(
+              'li',
+              null,
+              'Next'
+            )
+          )
+        )
       );
     }
   }]);
